dd69649fdc4f1a0db65d599f24015095ef95a31e
Jashwanth <jashwanthsariputi.com>
2026-08-09 11:55:41 +0530

    Fix project source import path

--- app/app.py
+++ app/app.py
@@ -1,3 +1,12 @@
+import sys
+import os
+
+PROJECT_ROOT = os.path.abspath(
+    os.path.join(os.path.dirname(__file__), "..")
+)
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
 import streamlit as st
 import pandas as pd
 
